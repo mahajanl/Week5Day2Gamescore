ebd6e5ef6e3c49b55855d5a17e66e1dc5cfa6ed4
mahajanl <lakshamima@gmail.com>
2017-10-24 15:10:28 -0400

    made models, scaffolded, added data

---
+++ Week5Day2/Models/Scores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Week5Day2.Models
+{
+    public class Scores
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Points { get; set; }
+
+
+    }
+}
---
+++ Week5Day2/Models/Week5Day2Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Week5Day2.Models
+{
+    public class Week5Day2Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Week5Day2Context() : base("name=Week5Day2Context")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Week5Day2.Models.Scores> Scores { get; set; }
+    }
+}
